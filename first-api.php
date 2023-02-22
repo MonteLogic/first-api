038aa4038fcde936c49674e96404164e1ebc840f
Monte Logic <dchavours@gmail.com>
2023-02-22 06:39:38 -0600

    Scaffolded get_option API but GET not working

--- first-api.php
+++ first-api.php
@@ -9,53 +9,90 @@
  *
  * @package         create-block
  */
-add_action('woocommerce_blocks_loaded', function() {
+add_action('woocommerce_blocks_loaded', function () {
     require_once __DIR__ . '/first-api-blocks-integration.php';
-	add_action(
-		'woocommerce_blocks_cart_block_registration',
-		function( $integration_registry ) {
-			$integration_registry->register( new FirstApi_Blocks_Integration() );
-		}
-	);
-	add_action(
-		'woocommerce_blocks_checkout_block_registration',
-		function( $integration_registry ) {
-			$integration_registry->register( new FirstApi_Blocks_Integration() );
-		}
-	);
+    add_action(
+        'woocommerce_blocks_cart_block_registration',
+        function ($integration_registry) {
+            $integration_registry->register(new FirstApi_Blocks_Integration());
+        }
+    );
+    add_action(
+        'woocommerce_blocks_checkout_block_registration',
+        function ($integration_registry) {
+            $integration_registry->register(new FirstApi_Blocks_Integration());
+        }
+    );
 });
+
+// Add options
+function add_options()
+{
+    add_option('my_boolean_value_1', false);
+    add_option('my_boolean_value_2', false);
+    add_option('my_boolean_value_3', false);
+}
+
+add_action('init', 'add_options');
 
 /**
  * Registers the slug as a block category with WordPress.
  */
-function register_FirstApi_block_category( $categories ) {
+function register_FirstApi_block_category($categories)
+{
     return array_merge(
         $categories,
         [
             [
-                'slug'  => 'first-api',
-                'title' => __( 'FirstApi Blocks', 'first-api' ),
+                'slug' => 'first-api',
+                'title' => __('FirstApi Blocks', 'first-api'),
             ],
         ]
     );
 }
-add_action( 'block_categories_all', 'register_FirstApi_block_category', 10, 2 );
+add_action('block_categories_all', 'register_FirstApi_block_category', 10, 2);
 
-
-
-add_action( 'rest_api_init', function () {
-    register_rest_route( 'my-api/v1', '/my-boolean-values', array(
+add_action('rest_api_init', function () {
+    register_rest_route('my-api/v1', '/my-boolean-values', array(
         'methods' => 'GET',
         'callback' => 'my_boolean_values_callback',
-    ) );
-} );
+    ));
+});
 
-
-function my_boolean_values_callback( $request ) {
+function my_boolean_values_callback($request)
+{
     $values = array(
-        'value1' => true,
-        'value2' => false,
-        'value3' => true,
+        'value1' => get_option('my_boolean_value_1'),
+        'value2' => get_option('my_boolean_value_2'),
+        'value3' => get_option('my_boolean_value_3'),
     );
     return $values;
+}
+
+add_action('rest_api_init', function () {
+    register_rest_route('my-api/v1', '/my-boolean-values', array(
+        'methods' => 'PUT',
+        'callback' => 'update_boolean_values',
+    ));
+});
+
+function update_boolean_values($request)
+{
+    // Retrieve the updated boolean values from the request body
+    $params = $request->get_params();
+    $value1 = $params['value1'];
+    $value2 = $params['value2'];
+    $value3 = $params['value3'];
+
+    // Perform any validation or processing on the values here
+
+    // Update the boolean values in the database or any other data source
+    // ...
+
+    // Return the updated boolean values as a JSON response
+    return array(
+        'value1' => $value1,
+        'value2' => $value2,
+        'value3' => $value3,
+    );
 }
--- src/js/checkout-newsletter-subscription-block/edit.js
+++ src/js/checkout-newsletter-subscription-block/edit.js
@@ -10,40 +10,70 @@ import {
 import { PanelBody } from '@wordpress/components';
 import { CheckboxControl } from '@woocommerce/blocks-checkout';
 import { getSetting } from '@woocommerce/settings';
+import { useEffect, useState } from '@wordpress/element';
 /**
  * Internal dependencies
  */
 import './style.scss';
-const { optInDefaultText } = getSetting( 'first-api_data', '' );
+const { optInDefaultText } = getSetting('first-api_data', '');
 
-export const Edit = ( { attributes, setAttributes } ) => {
+export const Edit = ({ attributes, setAttributes }) => {
 	const { text } = attributes;
 	const blockProps = useBlockProps();
+
+	useEffect(() => {
+		// Hard-code the boolean values to true
+		const updatedValues = {
+			value1: true,
+			value2: true,
+			value3: true
+		};
+
+		// Send a PUT request to update the values on the server
+		fetch('/wp-json/my-api/v1/my-boolean-values', {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(updatedValues)
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+			});
+	}, []);
+
+
 	return (
-		<div { ...blockProps }>
+		<div {...blockProps}>
+			{/* Edit the value of the API here. */}
+
+
+
+
 			<InspectorControls>
-				<PanelBody title={ __( 'Block options', 'first-api' ) }>
+				<PanelBody title={__('Block options', 'first-api')}>
 					Options for the block go here.
 				</PanelBody>
 			</InspectorControls>
 			<CheckboxControl
 				id="newsletter-text"
-				checked={ false }
-				disabled={ true }
+				checked={false}
+				disabled={true}
 			/>
 			<RichText
-				value={ text || optInDefaultText }
-				onChange={ ( value ) => setAttributes( { text: value } ) }
+				value={text || optInDefaultText}
+				onChange={(value) => setAttributes({ text: value })}
 			/>
 		</div>
 	);
 };
 
-export const Save = ( { attributes } ) => {
+export const Save = ({ attributes }) => {
 	const { text } = attributes;
 	return (
-		<div { ...useBlockProps.save() }>
-			<RichText.Content value={ text } />
+		<div {...useBlockProps.save()}>
+			<RichText.Content value={text} />
 		</div>
 	);
 };
